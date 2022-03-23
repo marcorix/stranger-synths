a7544532d81b2006d7d6bcd980a286990806e382
Marco Ricci <mr.ricci79@gmail.com>
2022-03-23 14:50:48 +0000

    edit update and show created

--- app/controllers/synths_controller.rb
+++ app/controllers/synths_controller.rb
@@ -1,7 +1,11 @@
 class SynthsController < ApplicationController
+  before_action :set_synth, only: [:show, :edit, :update, :destroy]
 
   def index
     @synths = Synth.all
+  end
+
+  def show
   end
 
   def new
@@ -11,11 +15,33 @@ class SynthsController < ApplicationController
   def create
     @synth = Synth.new(synth_params)
     @synth.user = current_user
-    @synth.save
+
+    if @synth.save
+      redirect_to @synt, notice: 'List was succesfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @synth.update(synth_params)
+    redirect_to @synth
+  end
+
+  def destroy
+    @synth.destroy
     redirect_to synths_path
   end
 
+
   private
+
+  def set_synth
+    @synth = Synth.find(params[:id])
+  end
 
   def synth_params
     params.require(:synth).permit(:name, :brand, :price, :year, :polyphony, :description, :conditions)
