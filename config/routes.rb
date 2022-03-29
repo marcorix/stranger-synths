02b5a1bf3549ff78a2be97ef6e01efffd5a7e0ce
Marco Ricci <mr.ricci79@gmail.com>
2022-03-29 12:07:27 +0100

    my bookings created

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'my-synths', to: 'pages#my_synths', as: :my_synths
+  get 'my-bookings', to: 'pages#my_bookings', as: :my_bookings
   resources :synths do
     resources :bookings, only: [:create]
   end
