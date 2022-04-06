bed4c5668f12c19227cdd5bdc537db3fd8a86170
Marco Ricci <mr.ricci79@gmail.com>
2022-04-06 14:15:41 +0100

    implemented calculateprice function for booking

--- app/javascript/controllers/booking_form_controller.js
+++ app/javascript/controllers/booking_form_controller.js
@@ -1,22 +1,22 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["price", "start", "end"];
+  static targets = ["totalPrice", "start", "end", "totalHide"];
   static values = { price: Number }
 
   // connect() {
   //   console.log(this.priceValue);
   // }
 
-  calculatePrice(e) {
+  calculatePrice() {
     const start_date = new Date(this.startTarget.value);
     const end_date = new Date(this.endTarget.value);
 
     if (start_date > 0 && end_date > 0) {
       const totalDays = (end_date.getTime() - start_date.getTime()) / (1000 * 60 * 60 * 24);
       const totalPrice = totalDays * this.priceValue;
-      this.priceTarget.innerHTML = `Total Price: £ ${totalPrice}`;
-      console.log(totalPrice);
+      this.totalPriceTarget.innerHTML = `Total Price: £ ${totalPrice}`;
+      this.totalHideTarget.value = totalPrice;
     }
   }
 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_05_093435) do
+ActiveRecord::Schema.define(version: 2022_04_06_114536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2022_04_05_093435) do
     t.bigint "synth_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "total_price"
     t.index ["synth_id"], name: "index_bookings_on_synth_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
